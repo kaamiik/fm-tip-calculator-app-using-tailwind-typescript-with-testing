b01e7be3973df64d32681341192c53d5a526bb06
Kamran Kiani <kiani.km@gmail.com>
2025-11-19 11:04:37 +0330

    Initialize project with React, TypeScript, and Tailwind
    Set up the initial project structure including configuration files, assets, and dependencies for a React app using TypeScript and Tailwind CSS. Added ESLint and Vitest for linting and testing, and included starter README files and style guide.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <header className="mt-12">
+        <a href="/" aria-labelledby="home">
+          <span id="home" hidden>
+            SPLITTER HOME
+          </span>
+          <img src="/images/logo.svg" alt="" className="mx-auto" />
+        </a>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
